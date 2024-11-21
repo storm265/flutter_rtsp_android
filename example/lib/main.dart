5814ec4339cf5e38f850e807916dfc3e0d3581b5
storm265 <peter4533@mail.ru>
2024-11-21 13:24:18 +0300

    updated added switch buttons

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:rtsp_ffmpeg/rtsp_ffmpeg.dart';
@@ -20,29 +19,52 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: GestureDetector(
-          child: RtspFFMpeg(
-            createdCallback: (controller) async {
-              final streamWatchdogPrinter = Timer.periodic(
-                  const Duration(milliseconds: 100),
-                  (timer) =>
-                      print("Stream is alive: ${controller.isStreamAlive}"));
+        floatingActionButton: Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await rtspController.stop();
+                await rtspController.play(
+                  "rtsp://192.168.65.122:8555/test",
+                ); // rws url
+              },
+              child: Text('Set rws url'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await rtspController.stop();
+                await rtspController.play(
+                  "rtsp://192.168.65.122:8554/operator/h264/720p",
+                ); // classic url
+              },
+              child: Text('Set classic url'),
+            ),
+            SizedBox(width: 20),
+          ],
+        ),
+        body: RtspFFMpeg(
+          createdCallback: (controller) async {
+            Timer.periodic(
+              const Duration(milliseconds: 100),
+              (timer) => print(
+                "Stream is alive: ${controller.isStreamAlive}",
+              ),
+            );
 
-              final rtsp = 'rtsp://192.168.65.122:8554/operator/h264/720p';
-              await controller.play(rtsp);
-              controller.streamAliveWatcher.listen((bool alive) async {
-                if (alive) {
-                  print("Stream is alive again");
-                } else {
-                  print("Stream is dead. Restarting...");
-                  await controller.stop();
-                  await controller.play(rtsp);
-                }
-              });
-
-              // await controller.play('rtsp://192.168.65.122:8554/test');
-            },
-          ),
+            final rtsp = 'rtsp://192.168.65.122:8554/operator/h264/720p';
+            await controller.play(rtsp);
+            controller.streamAliveWatcher.listen((bool alive) async {
+              if (alive) {
+                print("Stream is alive again");
+              } else {
+                print("Stream is dead. Restarting...");
+                await controller.stop();
+                await controller.play(rtsp);
+              }
+            });
+          },
         ),
       ),
     );
